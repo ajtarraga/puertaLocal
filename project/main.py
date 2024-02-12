2d1650c392776631fc3d9277ce14907b7a68ce46
ajtarraga <antoniojoaquin231999@gmail.com>
2024-02-12 21:10:35 +0100

    redirect to login

--- project/main.py
+++ project/main.py
@@ -6,7 +6,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('auth.login'))
 
 @main.route('/profile')
 @login_required
