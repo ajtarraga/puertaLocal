58c3eeebb98c3e206dc3f9d1dd34d503a1a7e3ac
ajtarraga <antoniojoaquin231999@gmail.com>
2024-02-12 20:42:28 +0100

    Change get ip

--- project/__init__.py
+++ project/__init__.py
@@ -39,13 +39,12 @@ def create_app():
 
 def get_ip_address():
     try:
-        # Create a socket object
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # Connect to a remote server (doesn't have to be reachable)
-        s.connect(('8.8.8.8', 80))
-        # Get the socket's local address, which should be your IP address
-        ip_address = s.getsockname()[0]
-        return ip_address
+        import socket
+        h_name = socket.gethostname()
+        IP_addres = socket.gethostbyname(h_name)
+        print("Host Name is:" + h_name)
+        print("Computer IP Address is:" + IP_addres)
+        return IP_addres
     except socket.error:
         return None
 
